c7cb7f6b82a9a73a63bca419387127a135b5c786
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-08 16:22:35 -0700

    Apply Combine Functions into Class

--- main.rb
+++ main.rb
@@ -1,9 +1,18 @@
-def draw_dark_mode_button(aButton)
-  # darken foreground color for dark mode
-  paint(aButton.label_text, aButton.x, aButton.y, aButton.foreground_color - 10, '#111111')
-end
-
-def draw_light_mode_button(aButton)
-  # lighten foreground color for non-dark mode
-  paint(aButton.label_text, aButton.x, aButton.y, aButton.foreground_color + 10, '#E0E0E0')
+class Button
+  def initialize(aButton)
+    @label_text = aButton.label_text
+    @x = aButton.x
+    @y = aButton.y
+    @foreground_color = aButton.foreground_color
+  end
+  
+  def draw_dark_mode
+    # darken foreground color for dark mode
+    paint(@label_text, @x, @y, @foreground_color - 10, '#111111')
+  end
+  
+  def draw_light_mode
+    # lighten foreground color for non-dark mode
+    paint(@label_text, @x, @y, @foreground_color + 10, '#E0E0E0')
+  end
 end
