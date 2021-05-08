cf8dd0486edabec0b74c123a72d20d6ca6f21129
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-08 16:17:36 -0700

    Apply Introduce Parameter Object

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
-def draw_dark_mode_button(label_text, x, y, foreground_color)
+def draw_dark_mode_button(aButton)
   # darken foreground color for dark mode
-  paint(label_text, x, y, foreground_color - 10, '#111111')
+  paint(aButton.label_text, aButton.x, aButton.y, aButton.foreground_color - 10, '#111111')
 end
 
-def draw_light_mode_button(label_text, x, y, foreground_color)
+def draw_light_mode_button(aButton)
   # lighten foreground color for non-dark mode
-  paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+  paint(aButton.label_text, aButton.x, aButton.y, aButton.foreground_color + 10, '#E0E0E0')
 end
